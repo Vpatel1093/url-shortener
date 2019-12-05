d91cbd001360638a40efa168ce0b805232a92f33
Vishal Patel <v.patel1093@gmail.com>
2019-12-05 08:35:39 -0500

    Add views, add bootstrap, style views, and add js functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_classes(level)
+    classes = ['alert', 'alert-dismissable']
+    classes <<
+      case level.to_s
+        when 'error' then 'alert-danger'
+        when 'success' then 'alert-success'
+        when 'alert', 'warn' then 'alert-warning'
+        when 'info' then 'alert-info'
+      end
+
+    classes.join(' ').strip
+  end
 end
